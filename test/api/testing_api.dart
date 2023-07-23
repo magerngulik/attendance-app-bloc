4bfd0c1d9ff37bcda59582baa1134f07d48a45ee
magerngulik <magerngulik@gmail.com>
2023-07-23 10:41:58 +0700

    update data

--- test/api/testing_api.dart
+++ test/api/testing_api.dart
@@ -1,10 +1,8 @@
-import 'package:attendance_app/data/services/services_attendance.dart';
-import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  Dio dioClient = Dio();
-  var data = ServiceAttandance(dioClient: dioClient);
+  // Dio dioClient = Dio();
+  // var data = ServiceAttandance(dioClient: dioClient);
 
   test('api get destination', () async {
     // var response = await data.action("1");
